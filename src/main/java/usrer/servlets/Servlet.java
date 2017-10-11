3c13c9519fa59f9f5a85ea39af2ac105446c0b2c
pomanu4 <pomanu4@ukr.net>
2017-10-11 12:44:33 +0300

    webapp

---
+++ src/main/java/usrer/servlets/Servlet.java
@@ -0,0 +1,22 @@
+package usrer.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "Servlet1", urlPatterns = { "/Servlet1" })
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+//		request.getRequestDispatcher("/WEB-INF/pages/page2.html").forward(request, response);
+		response.getWriter().print("servlet massage");
+	}
+
+}
